9ce3a2c333f50ad4e964f213f58c32fa6c55a4d5
vrnorbi <vrnorbi@gmail.com>
2020-10-17 10:34:02 +0200

    SearchConditions and init changed 2020.10.17

--- src/main/java/com/webler/inventory/model/dto/SearchConditions.java
+++ src/main/java/com/webler/inventory/model/dto/SearchConditions.java
@@ -1,5 +1,84 @@
 package com.webler.inventory.model.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class SearchConditions {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+
+    private String category;
+
+    private String brand;
+
+    private Integer price;
+
+    private String supplier;
+
+    private String manufaturer;
+
+    public SearchConditions() {
+    }
+
+    public SearchConditions(String name, String category, String brand, Integer price, String supplier, String manufaturer) {
+        this.name = name;
+        this.category = category;
+        this.brand = brand;
+        this.price = price;
+        this.supplier = supplier;
+        this.manufaturer = manufaturer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getManufaturer() {
+        return manufaturer;
+    }
+
+    public void setManufaturer(String manufaturer) {
+        this.manufaturer = manufaturer;
+    }
 }
---
+++ src/main/resources/init.sql
@@ -0,0 +1,36 @@
+
+Brand
+insert into brand VALUES(1, 'Acer');
+insert into brand VALUES(2, 'GeFors');
+insert into brand VALUES(3, 'Asus');
+insert into brand VALUES(4, 'Serioux');
+insert into brand VALUES(5, 'HP');
+insert into brand VALUES(6, 'IT_Store');
+insert into brand VALUES(7, 'IT_Factory');
+insert into brand VALUES(8, 'SmartPc');
+insert into brand VALUES(9, 'Dell');
+insert into brand VALUES(10, 'Lenovo');
+
+Category
+insert into category VALUES(1, 'Asztali_számítógép');
+insert into category VALUES(2, 'Monitorok');
+insert into category VALUES(3, 'Gamer_Pc');
+insert into category VALUES(4, 'Gamer_laptopok');
+insert into category VALUES(5, 'Gamer_monitorok');
+insert into category VALUES(6, 'Gamer_billentyűzetek');
+insert into category VALUES(7, 'Gamer_egér');
+insert into category VALUES(8, 'Szoftverek');
+insert into category VALUES(9, 'Pc_alkatrészek');
+insert into category VALUES(10, 'SSD_meghajtók');
+
+Products
+INSERT INTO product VALUES (1,'Acer Nitro 50 Gamer asztali számítógép, Intel® Core™ i7-8700, 8GB, 256GBB SSD, NVIDIA GeForce GTX 1060 6GB GDDR5, DVD író, WiFi AC, Windows® 10 Home',434.990,1,1);
+INSERT INTO product VALUES (2,'Acer VZ4860G All-in-One Asztali számítógép, Intel® Core™ i5-9400 akár 4.10 GHz processzorral, Coffee Lake, 23.8", Full HD, 8GB, 256GB SSD, Intel® UHD Graphics 630, Microsoft Windows 10 Pro',434.190,1,1);
+INSERT INTO product VALUES (3,'GeFors Office i7 Asztali PC Intel® Core™ i7-4770 3.90 Ghz, RAM 16GB, HDD 1000GB, VGA Intel® HD Graphics, DVD RW + Egér billentyűzet',154.990,2,1);
+INSERT INTO product VALUES (4,'ASUS ROG Strix G15DH-HU009D Asztali számítógép, AMD® Ryzen 5™ R5-3600X, 8GB, 512GB M.2 PCIE SSD, Nvidia GeForce RTX 2060 Super 8GB, Gigabit LAN, WiFi, Plexi oldalfal, FreeDOS, Fekete',379.990,3,3);
+INSERT INTO product VALUES (5,'Serioux Gaming asztali számítógép Intel® Core™ i5-9400F processzorral, max. 4.10GHz, 8GB DDR4, 1TB HDD, GeForce® GT 1030 2GB GDDR5',182.990,3,4);
+INSERT INTO product VALUES (6,'ASUS Cerberus gaming egér, Fekete',12.990,3,7);
+INSERT INTO product VALUES (7,'ASUS ROG STRIX SCOPE TKL RGB mechanikus gamer billentyűzet, magyar kiosztás',54.990,3,6);
+INSERT INTO product VALUES (8,'HP S700 Solid-State Drive (SSD meghajtó), 250GB, 2.5", SATA III',17.170,5,9);
+INSERT INTO product VALUES (9,'Dell Alienware AW3420DW Ívelt Gaming monitor, Nano IPS, 34", 120Hz, 2 ms, G-Sync, Ultra Wide QHD, 3440x1440, Display Port, Fekete/Szürke',369.990,9,5);
+INSERT INTO product VALUES (10,'Acer Predator Gaming LED IPS Monitor, 27", UHD, Display Port, HDMI, G-Sync, USB 3.0, Fekete',301.990,1,5);
