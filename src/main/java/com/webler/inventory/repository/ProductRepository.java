f1c54e922cc470bc6448d30193ea52790ed28ef0
etiennecouturier <test4Uniszs>
2020-10-28 08:29:44 +0100

    add product endpoints

--- src/main/java/com/webler/inventory/repository/ProductRepository.java
+++ src/main/java/com/webler/inventory/repository/ProductRepository.java
@@ -5,8 +5,4 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 //https://docs.spring.io/spring-data/jpa/docs/1.5.0.RELEASE/reference/html/jpa.repositories.html
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
-
-    Iterable<Product> findByNameContaining(String name);
-
-}
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer>, JpaSpecificationExecutor<Product> {}
