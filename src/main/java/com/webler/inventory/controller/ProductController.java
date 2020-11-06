b4dab6f7cc31cf406102c965cea52993f9de77f1
etiennecouturier <iistvan.szabo2@gmail.com>
2020-11-06 15:17:27 +0100

    fix

--- src/main/java/com/webler/inventory/controller/ProductController.java
+++ src/main/java/com/webler/inventory/controller/ProductController.java
@@ -4,10 +4,14 @@ import com.webler.inventory.model.dtos.FilterParams;
 import com.webler.inventory.model.dtos.PagingParams;
 import com.webler.inventory.model.dtos.SortingParams;
 import com.webler.inventory.model.entities.Product;
+import com.webler.inventory.model.entities.ProductHistory;
+import com.webler.inventory.repository.ProductHistoryRepository;
 import com.webler.inventory.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
 
 import static com.webler.inventory.repository.specs.ProductSpecifications.getProductsByFilterSpec;
 import static org.springframework.data.domain.PageRequest.of;
@@ -19,6 +23,9 @@ public class  ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private ProductHistoryRepository productHistoryRepository;
 
     @GetMapping(path = "/id")
     public @ResponseBody Product getProductById(Integer id) throws Exception {
@@ -41,6 +48,12 @@ public class  ProductController {
     @PostMapping(path = "/new")
     public void saveProduct(@RequestBody Product product) {
         productRepository.save(product);
+        ProductHistory productHistory = new ProductHistory();
+        productHistory.setDate(new Date(System.currentTimeMillis()));
+        productHistory.setPrice(product.getPrice());
+        productHistory.setQuantity(product.getQuantity());
+        productHistory.setProduct(product);
+        productHistoryRepository.save(productHistory);
     }
 
     @PutMapping(path = "/update")
